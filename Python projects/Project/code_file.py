ba243f2417f1044355e547bd0d235e157fb7a2c9
Leevi Malin <leevi.malin@netikka.fi>
2024-05-08 16:53:04 +0300

    save plots and tensorflow pred model

--- Python projects/Project/code_file.py
+++ Python projects/Project/code_file.py
@@ -3,9 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn as skl
 import sklearn.linear_model as skl_lm
+import sklearn.metrics as skl_m
 from scipy import stats
-from functools import wraps
 from time import perf_counter
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
 
 
 NEWSGROUPTRANSFORMER = {
@@ -29,75 +32,94 @@ NEWSGROUPTRANSFORMER = {
     18 : "talk.politics.mideast",
     19 : "talk.politics.misc",
     20 : "talk.religion.misc"
-}
+    }
 
-def readFile():
-    return pd.read_csv("/Users/leevimalin/Library/Mobile Documents/com~apple~CloudDocs/Opinnot/Matikka/Tilastot ja ohjelmistot/Python/Project/harjoitustyodata.csv")
+def makeSave(name: str):
+    '''Saves current plot to given location'''
+    plt.savefig("/Users/leevimalin/Library/Mobile Documents/com~apple~CloudDocs/Opinnot/Ohjelmointi/Python projects/Project/plots/"+name+".pdf")
 
+#**********
+# Part 1
+#**********
 def part1(df):
-    # 
     list_of_df = []
-    list_of_plot = []
     partdf, partplt = calculatePart1(df,"freedom",[12,17,18,19])
     list_of_df.append(partdf)
-    list_of_plot.append(partplt)
+    makeSave("part1hist1")
     partdf, partplt = calculatePart1(df,"nation",[17,18,19])
     list_of_df.append(partdf)
-    list_of_plot.append(partplt)
+    makeSave("part1hist2")
     partdf, partplt = calculatePart1(df,"logic",[1,13,19,20])
     list_of_df.append(partdf)
-    list_of_plot.append(partplt)
+    makeSave("part1hist3")
     partdf, partplt = calculatePart1(df,"normal",[2,6,13,14])
     list_of_df.append(partdf)
-    list_of_plot.append(partplt)
+    makeSave("part1hist4")
     partdf, partplt = calculatePart1(df,"program",[2,6,5,19])
     list_of_df.append(partdf)
-    list_of_plot.append(partplt)
+    makeSave("part1hist5")
+    plt.close()
 
-    return list_of_df, list_of_plot
+    return list_of_df
 
+#**********
+# Part 2
+#**********
 def part2(df, group1: int, group2: int):
     '''Calculates part 2 things for two(2) given groups''' 
-    # Get message rows for groups
-    gr1rows = getGroupRows(df,group1)
-    gr2rows = getGroupRows(df,group2)
     
     # get total word count of each message
-    wordCount1 = getAllWordCount(df,gr1rows)
-    wordCount2 = getAllWordCount(df,gr2rows)
+    wordCount1 = np.array(getAllWordCount(df,group1))
+    wordCount2 = np.array(getAllWordCount(df,group2))
 
-    lgwordCount1 = convertToLog(wordCount1)
-    lgwordCount2 = convertToLog(wordCount2)
+    # Avoid -inf when trying to log 0
+    lgwordCount1 = np.log10(wordCount1, out=np.zeros_like(wordCount1), where=(wordCount1!=0))
+    lgwordCount2 = np.log10(wordCount2, out=np.zeros_like(wordCount2), where=(wordCount2!=0))
 
-    plt.figure(1)
-    lghistgr1 = plt.hist(lgwordCount1,80)
-    plt.figure(2)
-    lghistgr2 = plt.hist(lgwordCount2,80)
+    plt.figure(figsize=(20,15))
+    plt.hist(lgwordCount1,80)
+    makeSave("part2hist1")
+    plt.figure(figsize=(20,15))
+    plt.hist(lgwordCount2,80)
+    makeSave("part2hist2")
+    plt.close()
 
     tTesti = stats.ttest_ind(wordCount1,wordCount2)
-    return lghistgr1, lghistgr2, tTesti
+    return tTesti
 
+#**********
+# Part 3
+#**********
 def part3(df):
     '''Calculate part 3 things'''
     group1 = 2
     group2 = 16
 
     allSecFromNight = np.array(df['secsfrommidnight'])
-    plt.figure(1)
+    plt.figure(figsize=(20,15))
     plt.hist(allSecFromNight,200)
-    
+    makeSave("part3hist1")
 
     allSecFrom8am = np.array(df['secsfrom8am'])
     mean, median, deviation = calculatePart3(allSecFrom8am)
-    plt.figure(2)
+    
+    plt.figure(figsize=(20,15))
     plt.hist(allSecFrom8am,200)
+    makeSave("part3hist2")
     
     gr1rows = getGroupRows(df,group1)
     gr2rows = getGroupRows(df,group2)
     
     meangr1 = np.mean(getRangeFromColumn(df,gr1rows,"secsfrom8am"))
-    meangr2 = np.mean(getRangeFromColumn(df,gr1rows,"secsfrom8am"))
+    meangr2 = np.mean(getRangeFromColumn(df,gr2rows,"secsfrom8am"))
 
+    res_list = [mean, median, deviation, meangr1, meangr2]
+
+    return res_list
+
+#**********
+# Part 4
+#**********
 def part4(df, word1:str , word2: str, group=None):
     
     if group == None:
@@ -114,9 +136,12 @@ def part4(df, word1:str , word2: str, group=None):
 
         return np.corrcoef(range1,range2)
 
+#**********
+# Part 5
+#**********
 def part5(df):
 
-    # Check for normal distribution
+  # Check for normal distribution
     sentValues = np.array(df["meanvalences"])
     corValues = stats.normaltest(sentValues)
 
@@ -127,11 +152,13 @@ def part5(df):
     three_highest = sortedList[-3:]
 
     #Draw histograms
-    histList = []
-    figurenum = 1
+    fignum = 1
     for sent in sentList:
-        histList.append(plt.hist(sent,15))
-        figurenum += 1
+        plt.figure()
+        plt.hist(sent,15)
+        makeSave("part5hist"+str(fignum))
+        fignum += 1
+        plt.close()
     
     # Calculate differences
     diff1 = stats.ttest_ind(sentList[3],sentList[4])
@@ -143,37 +170,80 @@ def part5(df):
         "Sentimenttien jakauma" : statList,
         "Kolme suurinta" : three_highest,
         "Kolme pienintä" : three_lowest,
-        "Histogrammit" : histList,
         "pc ja mac ero" : diff1,
         "baseball ja hockey ero" : diff2,
         "autos ja motorccyles ero" : diff3
     }
     return part5results
 
+#**********
+# Part 6
+#**********
 def part6(df):
+    # select the data we need in this part
+    partdata = df.loc[(df["groupID"] == 2) | (df["groupID"] == 15)]
+    # Create new array: if comp.graphics then 1 else -1
+    target = partdata['groupID'].apply(lambda x: 1 if x == 2 else -1)
 
-    rows1 = np.array(getGroupRows(df,2))
-    rows2 = np.array(getGroupRows(df,15))
-    # tää on hiton tyhmä tapa tehä tätä mut fuck numpy ja sen tietorakenteet
-    words = pd.DataFrame()
-    words["X"] = df.loc[(df["groupID"] == 2) | (df["groupID"] == 15),"jpeg"]
-    #words2 = df.loc[df["groupID"] == 15,["jpeg","freedom"]]
-
-    print(words["X"])
+    mse1 = makeNewPredict(partdata,["jpeg"],target)
+    mse2 = makeNewPredict(partdata,["jpeg","earth"],target)
     
-    target = []
-    #training = []
-    for num in rows1:
-        #training.append(df.at[num,"jpeg"])
-        target.append(1)
-    for num in rows2:
-        #training.append(df.at[num,"jpeg"])
-        target.append(-1)
+    grpred = {
+    "msegr1" : makeNewPredict(partdata, ["group1"], target),
+    "msegr2" : makeNewPredict(partdata, ["group2"], target),
+    "msegr3" : makeNewPredict(partdata, ["group3"], target),
+    "msegr4" : makeNewPredict(partdata, ["group4"], target),
+    "msegr5" : makeNewPredict(partdata, ["group5"], target),
+    "msegr6" : makeNewPredict(partdata, ["group6"], target),
+    "msegr7" : makeNewPredict(partdata, ["group7"], target),
+    "msegr8" : makeNewPredict(partdata, ["group8"], target)
+    }
 
+    answlist = [mse1, mse2, grpred]
+
+    return answlist
+
+def makeNewPredict(df, words: list[str], target):
+    '''Creates model for predicting group from fordcounts using linear regression model
     
-    tulos = skl_lm.LinearRegression().fit(np.array(words["X"]),target)
+    params: 
+        words: array of words for used in creating prediction model
+        target: Target array of set values for differentiating data
+    '''
+    wordsdf = df[words]
+    
+    # Create new prediction model and train it with given data
+    tulos = skl_lm.LinearRegression().fit(wordsdf,target)
+    pred = tulos.predict(wordsdf)
+    mse = skl_m.mean_squared_error(target,pred)
 
-    ypred = tulos.predict()
+    return mse
+
+def tensorThings(df):
+    '''Calculates part 6 things with tensor flow'''
+    # Select the data we need in this part
+    partdata = df.loc[(df["groupID"] == 2) | (df["groupID"] == 15)]
+    target = partdata['groupID'].apply(lambda x: 1 if x == 2 else 0)
+    # Split data to train and test
+    X_train, X_test, y_train, y_test = train_test_split(partdata[["jpeg","earth"]], target, test_size=0.2, random_state=42)
+
+    # create model
+    model = Sequential()
+    # Add hidden layers to model
+    model.add(Dense(units=32, activation='relu', input_dim=len(X_train.columns)))
+    model.add(Dense(units=64, activation='relu'))
+    model.add(Dense(units=1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='sgd', metrics='accuracy')
+    model.fit(X_train, y_train, epochs=200, batch_size=32)
+
+    y_hat = model.predict(X_test)
+    mse = skl_m.mean_squared_error(y_test, y_hat)
+
+
+
+    eval = model.evaluate(X_test, y_test, batch_size=32)
+
+    return mse, eval
 
 def calculatePart1(df,word: str,groups: list) -> list:
     '''
@@ -183,17 +253,19 @@ def calculatePart1(df,word: str,groups: list) -> list:
     totalWordCounts = {}
     statlist = {}
     for num in groups:
-        # find list of rows for certain group
-        rowlist = getGroupRows(df,num)
         # find word counts for messages in group 
-        countList = getIndWordCount(df,rowlist,word)
-        # add wor count sums to list for later use
+        countList = getIndividualWordCount(df,num,word)
+        # add word count sums to list
         totalWordCounts[NEWSGROUPTRANSFORMER[num]] = np.sum(countList)
         # save stats for word counts
         statlist[NEWSGROUPTRANSFORMER[num]] = getStats(countList,0.001,0.999)
 
     statdf = pd.DataFrame(statlist)
-    histplt = plt.bar(totalWordCounts.keys(),totalWordCounts.values())  
+
+    cm=1/2.54
+    plt.figure(figsize=(20*cm,15*cm))
+    histplt = plt.bar(totalWordCounts.keys(),totalWordCounts.values())
+ 
     return statdf, histplt
 
 def calculatePart3(data: list):
@@ -214,7 +286,10 @@ def calculatePart5(df):
 
     return sentList, statList
 
+
 def takeFirstFromDict(dictList: list[dict]):
+    '''Takes the first element of dict and returns it
+    (there propably is better way for this but...)'''
     firstList = []
     for member in dictList:
         firstList.append(member["median"])
@@ -223,6 +298,7 @@ def takeFirstFromDict(dictList: list[dict]):
 
 def getRangeFromColumn(df, rowlist, column: str) -> np.array:
     '''
+    Returns values from given column for given row indexes
     Params:
         rowlist: list of rows from function getGroupRows()
         column: Name of column where values are searched
@@ -230,18 +306,11 @@ def getRangeFromColumn(df, rowlist, column: str) -> np.array:
     return np.array(df.loc[rowlist[0]:rowlist[-1], column])
 
 
-def getRowWordCount(df,row):
-    # Counts all words in single row
-    wordcount = df.iloc[row, 5:963].sum()
-    return wordcount
+def getAllWordCount(df,group: int):
+    '''Returns sum of all words for given group'''
 
-def getAllWordCount(df,groups: list) -> list:
-    # iterates over list of rows and calculates
-    # all words in their messages
-    wordlist = []
-    for row in groups:
-        wordlist.append(getRowWordCount(df,row))
-    return wordlist
+    return df[df["groupID"] == group][5:965].sum()
+
 
 def convertToLog(input: list):
         np.log10(input)
@@ -259,8 +328,10 @@ def getStats(list: list, lowquantile: float, highquantile: float) -> dict:
     return stats
 
 def getGroupRows(df,group: int) -> list:
-    # finds row indexes for given group
-    # returns list of row indexes
+    '''
+    finds row indexes for given group
+    returns list of row indexes
+    '''
     index = 0
     rowList = []
     for ID in df["groupID"]:
@@ -269,32 +340,39 @@ def getGroupRows(df,group: int) -> list:
         index += 1
     return rowList
 
-def getIndWordCount(df,rowlist: list[int],word: str) -> list:
+def getIndividualWordCount(df,group: int,word: str):
+    '''Finds all groups messages word count for single word'''
     
-    # Finds all word counts for a newsGroup,word pair
-    # Returns vector with indivitual message wordcounts
-    wordCounts = []
-    for num in rowlist:
-        wordCounts.append(df.at[num,word])
-    
-    return wordCounts
+    return df.loc[df["groupID"] == group, word]
 
+def allPart4(data):
+    '''Calculates all part 4 things and returns them as a list of correlation coefficients'''
+    corrList = []
+
+    corrList.append(part4(data, "jpeg", "gif"))
+    corrList.append(part4(data, "write", "sale"))
+    corrList.append(part4(data, "jpeg", "gif", 2))
+
+    return corrList
 
 def main():
     T1 = perf_counter()
 
-    data = readFile()
-    #part1Df ,part1Plt = part1(data)
-    #lghist11, lghist12, tTesti1 = part2(data,10,11)
-    #lghist21, lghist22, tTesti2 = part2(data,8,9)
-    #part3(data)
-    #corr1 = part4(data, "jpeg", "gif")
-    #corr2 = part4(data, "write", "sale")
-    #corr3 = part4(data, "jpeg", "gif", 2)    
-    #part5(data)
-    part6(data)
+    data = pd.read_csv("/Users/leevimalin/Library/Mobile Documents/com~apple~CloudDocs/Opinnot/Ohjelmointi/Python projects/Project/harjoitustyodata.csv")
 
-    testteii = np.array(data.loc[data["groupID"]==12,"jpeg"])
+    '''
+    results = {
+        "Part1 results" : part1(data),
+        "Part2 results1" : part2(data,10,11),
+        "Part2 results2" : part2(data,8,9),
+        "Part3 results" : part3(data),
+        "Part4 results" : allPart4(data),
+        "Part5 results" : part5(data),
+        "Part6 results" : part6(data)
+    }
+    '''
+
+    mse, eval = tensorThings(data)
 
     T2 = perf_counter()
     print()
@@ -303,3 +381,5 @@ def main():
     
 if __name__ == "__main__":
     main()
+
+#end of code_file.py
